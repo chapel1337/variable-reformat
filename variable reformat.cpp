720d8bb2ba34a8b390ca1d95e6794a72344aea88
chapel1337 <103341366+chapel1337@users.noreply.github.com>
2022-09-05 10:20:02 -0400

    Add files via upload

---
+++ variable reformat.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+void reformat(std::string theString)
+{
+    unsigned int equal{ theString.find('=', 0) };
+    unsigned int length{ theString.length() };
+
+    std::string reformattedString{ theString };
+    
+    reformattedString[equal] = '{';
+    reformattedString += " }";
+    
+    std::cout << reformattedString << std::endl;
+}
+
+std::string read()
+{
+    // credit to ROMAHKAO
+    std::string s{};
+    std::getline(std::cin >> std::ws, s);
+    return s;
+}
+
+int main()
+{
+    std::string variableString{ "unknown" };
+
+    std::cout << "enter a line of code: " << std::endl;
+    variableString = read();
+
+    reformat(variableString);
+
+    main();
+}
